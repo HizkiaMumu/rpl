17f083ebf690c7959082062e4ce265e9fe4e0f8e
Hizkia Mumu <hizkiamumu24@gmail.com>
2018-05-09 21:08:36 +0800

    add homepage template

---
+++ resources/views/master.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from themes.iki-bir.com/brailie/index25.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 05 May 2018 08:42:04 GMT -->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="text" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="/style/images/favicon.png">
+<title>@yield('title')</title>
+<link rel="stylesheet" type="text/css" href="/style/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/style/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="/style/revolution/css/settings.css">
+<link rel="stylesheet" type="text/css" href="/style/revolution/css/layers.css">
+<link rel="stylesheet" type="text/css" href="/style/revolution/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="/style/revolution/revolution-addons/filmstrip/css/revolution.addon.filmstrip.css">
+<link rel="stylesheet" type="text/css" href="/style/revolution/revolution-addons/typewriter/css/typewriter.css">
+<link rel="stylesheet" type="text/css" href="/style/type/icons.css">
+<link rel="stylesheet" type="text/css" href="/style.css">
+<link rel="stylesheet" type="text/css" href="/style/css/color/oasis.css">
+<link href='https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i' rel='stylesheet' type='text/css'>
+@yield('style')
+</head>
+<body class="onepage" data-spy="scroll" data-target=".navbar">
+
+  <div class="content-wrapper">
+
+    <nav class="navbar transparent absolute nav-wrapper-dark inverse-text navbar-expand-lg">
+    <div class="container">
+      <div class="navbar-header">
+        <div class="navbar-brand"><a href="index.html"><img src="#" srcset="/logo/2.png 1x, /logo/2.png 2x" alt="" style="height: 60px; width: auto;"></a></div>
+        <div class="navbar-hamburger ml-auto d-lg-none d-xl-none">
+          <button class="hamburger animate" data-toggle="collapse" data-target=".navbar-collapse"><span></span></button>
+        </div>
+      </div>
+      <!-- /.navbar-header -->
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav ml-auto" data-smartmenus-id="1525870029580567">
+          <li class="nav-item">
+            <a class="nav-link has-submenu" href="#" id="sm-1525870029580567-1" aria-haspopup="true" aria-controls="sm-1525870029580567-2" aria-expanded="false">Home<span class="sub-arrow"></span></a>
+          </li>
+          <li class="nav-item dropdown"><a class="nav-link has-submenu" href="#" id="sm-1525870029580567-3" aria-haspopup="true" aria-controls="sm-1525870029580567-4" aria-expanded="false">Tentang Kami<span class="sub-arrow"></span></a>
+          </li>
+          <li class="nav-item dropdown"><a class="nav-link has-submenu" href="#" id="sm-1525870029580567-21" aria-haspopup="true" aria-controls="sm-1525870029580567-22" aria-expanded="false">Portfolio<span class="sub-arrow"></span></a>
+          </li>
+          <li class="nav-item dropdown"><a class="nav-link has-submenu" href="#" id="sm-1525870029580567-39" aria-haspopup="true" aria-controls="sm-1525870029580567-40" aria-expanded="false">Blog<span class="sub-arrow"></span></a>
+          </li>
+          <li class="nav-item dropdown"><a class="nav-link has-submenu" href="#" id="sm-1525870029580567-55" aria-haspopup="true" aria-controls="sm-1525870029580567-56" aria-expanded="false">Gallery<span class="sub-arrow"></span></a>
+          </li>
+          <li class="nav-item"><a class="nav-link has-submenu" href="#" id="sm-1525870029580567-65" aria-haspopup="true" aria-controls="sm-1525870029580567-66" aria-expanded="false">Podcast<span class="sub-arrow"></span></a>
+          </li>
+        </ul>
+        <!-- /.navbar-nav -->
+        <div class="navbar-divider"></div>
+        <ul class="social social-mute social-s">
+          <li><a href="#"><i class="fa fa-github"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+  </nav>
+
+    @yield('content')
+
+  </div>
+
+<!-- /.content-wrapper -->
+<script src="/style/js/jquery.min.js"></script>
+<script src="/style/js/popper.min.js"></script>
+<script src="/style/js/bootstrap.min.js"></script>
+<script src="/style/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="/style/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="/style/revolution/revolution-addons/filmstrip/js/revolution.addon.filmstrip.min.js"></script>
+<script src="/style/revolution/revolution-addons/typewriter/js/revolution.addon.typewriter.min.js"></script>
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems! The following part can be removed on Server for On Demand Loading) -->
+<!-- <script src="/style/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="/style/revolution/js/extensions/revolution.extension.video.min.js"></script> -->
+<script src="/style/js/plugins.js"></script>
+<script src="/style/js/scripts.js"></script>
+@yield('script')
+</body>
+
+<!-- Mirrored from themes.iki-bir.com/brailie/index25.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 05 May 2018 08:46:05 GMT -->
+</html>
